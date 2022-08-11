169f5cd098ed195eb1f1ec12adb9f68995f287c9
linhung0319 <linhung0319@gmail.com>
2022-08-11 18:40:47 +0100

    feat: Add a new method to BookItem
    Add the checkout method to BookItem. Once checkout
    the BookItem, the status become loaned.

--- LMS/model.py
+++ LMS/model.py
@@ -1,3 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from LMS.constants import Address, BookStatus
 
 class Library():
@@ -5,6 +8,9 @@ class Library():
         self.__name = name
         self.__address = address
         self.__bookItems = bookItems
+
+    def __len__(self):
+        return len(self.__bookItems)
 
 class Book():
     def __init__(self, author='', ISBN='', 
@@ -16,28 +22,72 @@ class Book():
         self.__publisher = publisher
      
     @property
+    def author(self):
+        return self.__author
+    
+    @property
+    def ISBN(self):
+        return self.__ISBN
+
+    @property
     def title(self):
         return self.__title
+
+    @property
+    def subjects(self):
+        return self.__subjects
+
+    @property
+    def publisher(self):
+        return self.__publisher
+
 
 class BookItem(Book):
     def __init__(self, author='', ISBN='', 
                  title='', subjects='', publisher='',
                  bibNum='', publicationYear=None,
-                 borrowed=None, dueDate=None, 
-                 bookStatus: BookStatus=BookStatus.NONE):
+                 borrowedDate=None, dueDate=None, 
+                 bookStatus: BookStatus=BookStatus.AVAILABLE):
         super(BookItem, self).__init__(author, ISBN, title, 
                                        subjects, publisher)
         
         self.__bibNum = bibNum
         self.__publicationYear = publicationYear
         
-        self.__borrowed = borrowed
+        self.__borrowedDate = borrowedDate
         self.__dueDate = dueDate
         self.__bookStatus = bookStatus
+
+    def checkout(self):
+        if self.bookStatus == BookStatus.AVAILABLE:
+            self.bookStatus = BookStatus.LOANED
+            return True
+        else:
+            msg = """The book status is in {}. You can \ 
+                     not check out this book!!""".format(self.bookStatus)
+            logger.info(msg)
+            return False
 
     @property
     def bibNum(self):
         return self.__bibNum
 
-    def checkout(self):
-        pass
+    @property
+    def publicationYear(self):
+        return self.__publicationYear
+    
+    @property
+    def borrowedDate(self):
+        return self.__borrowedDate
+
+    @property
+    def dueDate(self):
+        return self.__dueDate
+
+    @property
+    def bookStatus(self):
+        return self.__bookStatus
+
+    @bookStatus.setter
+    def bookStatus(self, status: BookStatus):
+        self.__bookStatus = status
